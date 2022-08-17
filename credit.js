52f9e68925b793b16f3db19b157bbea6429801d3
SzymonW2001 <111139217+SzymonW2001@users.noreply.github.com>
2022-08-17 08:56:38 +0200

    Add files via upload

---
+++ credit.js
@@ -0,0 +1,700 @@
+
+
+
+questions = [
+    {
+        question: "What is it?",
+        answers: [
+            {
+            answer: "USA",
+            answerPicture:"./grafiki/wrong14.jpg",
+            answerPoints: -14
+            }, 
+            {
+            answer: "USB",
+            answerPicture:"./grafiki/neutral.jpg",
+            answerPoints: 0
+            },
+            {
+            answer: "Cringe",
+            answerPicture:"./grafiki/correct40.jpg",
+            answerPoints: 40
+            },
+            {
+            answer: "Mexico",
+            answerPicture:"./grafiki/wrong14.jpg",
+            answerPoints: -14
+            },
+        ],
+        questionPictures: ["./grafiki/USA.jpg"],
+        answerPictures: [],
+        imageClass: "long"
+    },
+    {
+        question: "Witch photo is real?",
+        answers: [],
+        questionPictures: [],
+        answerPictures: [
+            {
+                answer: "./grafiki/tanks.jpg",
+                answerPicture:"./grafiki/wrong1999.jpg",
+                answerPoints: -1999
+                }, 
+                {
+                answer: "./grafiki/no tanks.jpg",
+                answerPicture:"./grafiki/correct1488.jpg",
+                answerPoints: 1488
+                }
+        ],
+        imageClass: "medium"
+    },
+    {
+        question: "How much time do you spend playing computer games?",
+        answers: [
+            {
+            answer: "0-2 hours a week",
+            answerPicture:"./grafiki/correct678.jpg",
+            answerPoints: 678
+            }, 
+            {
+            answer: "2-3 hours a week ",
+            answerPicture:"./grafiki/correct3.jpg",
+            answerPoints: 3
+            },
+            {
+            answer: "more than 4 hours a week",
+            answerPicture:"./grafiki/wrong127.jpg",
+            answerPoints: -127
+            },
+            {
+            answer: "i have never seen computer in my life",
+            answerPicture:"./grafiki/correct1488.jpg",
+            answerPoints: 1488
+            },
+            {
+            answer: "3-4 hours daily",
+            answerPicture:"./grafiki/wrong299792458.jpg",
+            answerPoints: -299792458
+            },
+        ],
+        questionPictures: [],
+        answerPictures: [],
+    },
+    {
+        question: "Are these two similar?",
+        answers: [
+            {
+            answer: "absolutely not",
+            answerPicture:"./grafiki/correct1836.jpg",
+            answerPoints: 1836
+            }, 
+            {
+            answer: "yes",
+            answerPicture:"./grafiki/wrong668.jpg",
+            answerPoints: -668
+            },
+            {
+            answer: "mayby a bit",
+            answerPicture:"./grafiki/wrong14.jpg",
+            answerPoints: -14
+            },
+        ],
+        questionPictures: ["./grafiki/winnie.jpg", "./grafiki/xi.jpg"],
+        answerPictures: [],
+        imageClass: "medium"
+    },
+    {
+        question: "Is Taiwan a country?",
+        answers: [
+            {
+            answer: "No",
+            answerPicture:"./grafiki/correct9736.jpg",
+            answerPoints: 9736
+            }, 
+            {
+            answer: "Yes",
+            answerPicture:"./grafiki/wrong69420.jpg",
+            answerPoints: -69420
+            },
+            {
+            answer: "Yes (Peoples Republic of China)",
+            answerPicture:"./grafiki/correct69.jpg",
+            answerPoints: 69
+            },
+            {
+            answer: "There is no such thing as Taiwan",
+            answerPicture:"./grafiki/neutral.jpg",
+            answerPoints: 0
+        },
+        ],
+        questionPictures: [],
+        answerPictures: [],
+        imageClass: []
+
+    },
+    {
+        question: "Did something happen in 1989?",
+        answers: [
+            {
+            answer: "Nothing, very peaceful year",
+            answerPicture:"./grafiki/correct1836.jpg",
+            answerPoints: 1836
+            }, 
+            {
+            answer: "Yes it was [redacted]",
+            answerPicture:"./grafiki/wrong299792458.jpg",
+            answerPoints: -299792458
+            },
+            {
+            answer: "Perhaps",
+            answerPicture:"./grafiki/wrong668.jpg",
+            answerPoints: -668
+            },
+        ],
+        questionPictures: [],
+        answerPictures: [],
+        imageClass: []
+    },
+    {
+        question: "[Redacted]",
+        answers: [
+            {
+            answer: "Glory to CCP!",
+            answerPicture:"./grafiki/correct678.jpg",
+            answerPoints: 678
+            }, 
+            {
+            answer: "[Redacted]",
+            answerPicture:"./grafiki/neutral.jpg",
+            answerPoints: 0
+            },
+            {
+            answer: "[Redacted]",
+            answerPicture:"./grafiki/wrong1999.jpg",
+            answerPoints: 1999
+            }, 
+            {
+            answer: "[Redacted]",
+            answerPicture:"./grafiki/correct50000000000.jpg",
+            answerPoints: 5000000000
+            },
+        ],
+        questionPictures: ["./grafiki/west taiwan.jpg"],
+        answerPictures: [],
+        imageClass: ["long"]
+
+    },
+    {
+        question: "What is speciality of Wuhan Wet Market?",
+        answers: [],
+        questionPictures: [],
+        answerPictures: [
+            {
+            answer: "./grafiki/cat.jpg",
+            answerPicture:"./grafiki/wrong1.jpg",
+            answerPoints: -1
+            }, 
+            {
+            answer: "./grafiki/soup.jpg",
+            answerPicture:"./grafiki/wrong1.jpg",
+            answerPoints: -1
+            },
+            {
+            answer: "./grafiki/icecream.webp",
+            answerPicture:"./grafiki/wrong1.jpg",
+            answerPoints: -1
+            }, 
+            {
+            answer: "./grafiki/EO7uWn7X4AAlh2K.png",
+            answerPicture:"./grafiki/correct40.jpg",
+            answerPoints: 40
+            }
+        ],
+        imageClass: "small"
+
+    },
+    {
+        question: "Who was greatest chinese economist?",
+        answers: [],
+        questionPictures: [],
+        answerPictures: [
+            {
+            answer: "./grafiki/mao.jpg",
+            answerPicture:"./grafiki/correct1488.jpg",
+            answerPoints: 1488
+            }, 
+            {
+            answer: "./grafiki/deng.webp",
+            answerPicture:"./grafiki/correct40.jpg",
+            answerPoints: 40
+            }],
+        imageClass: "medium"
+
+
+    },
+    {
+        question: "Is china capitalist or communist nation?",
+        answers: [],
+        questionPictures: [],
+        answerPictures: [
+            {
+            answer: "./grafiki/com.png",
+            answerPicture:"./grafiki/correct1836.jpg",
+            answerPoints: 1836
+            }, 
+            {
+            answer: "./grafiki/cap.png",
+            answerPicture:"./grafiki/wrong668.jpg",
+            answerPoints: -668
+            }],
+        imageClass: "medium"
+
+    },
+    {
+        question: "Is this man handsome?",
+        answers: [
+            {
+            answer: "Yes",
+            answerPicture:"./grafiki/correct3.jpg",
+            answerPoints: 3
+            }, 
+            {
+            answer: "No",
+            answerPicture:"./grafiki/wrong127.jpg",
+            answerPoints: -127
+            }],
+        questionPictures: ["./grafiki/polpot.jpg"],
+        answerPictures: [],
+        imageClass: "long"
+    },
+    {
+        question: "Who is greatest leader in human history?",
+        answers: [],
+        questionPictures: [],
+        answerPictures: [
+            {
+            answer: "./grafiki/mao.jpg",
+            answerPicture:"./grafiki/correct1488.jpg",
+            answerPoints: 1488
+            }, 
+            {
+            answer: "./grafiki/prism.jpg",
+            answerPicture:"./grafiki/wrong1999.jpg",
+            answerPoints: -1999
+            },
+            {
+            answer: "./grafiki/xi.jpg",
+            answerPicture:"./grafiki/correct1836.jpg",
+            answerPoints: 1836
+            }, 
+            {
+            answer: "./grafiki/joe.jpg",
+            answerPicture:"./grafiki/wrong10000.jpg",
+            answerPoints: -10000
+            }, 
+        ],
+        imageClass: "small"
+
+    },
+    {
+        question: "How many children should decent citizen have?",
+        answers: [
+            {
+            answer: "1",
+            answerPicture:"./grafiki/wrong668.jpg",
+            answerPoints: -688
+            }, 
+            {
+            answer: "2",
+            answerPicture:"./grafiki/wrong49.jpg",
+            answerPoints: -49
+            },
+            {
+            answer: "3",
+            answerPicture:"./grafiki/correct1836.jpg",
+            answerPoints: 1836
+            }, 
+            {
+            answer: "4",
+            answerPicture:"./grafiki/wrong1.jpg",
+            answerPoints: -1
+            },
+            {
+            answer: "3/8",
+            answerPicture:"./grafiki/wrong69420.jpg",
+            answerPoints: -69420
+            }
+        ],
+        questionPictures: [],
+        answerPictures: [],
+        imageClass: []
+
+    },
+    {
+        question: "Do you know who Uyghurs are?",
+        answers: [
+            {
+            answer: "No",
+            answerPicture:"./grafiki/correct1836.jpg",
+            answerPoints: 1836
+            }, 
+            {
+            answer: "Wrong answer",
+            answerPicture:"./grafiki/wrong14.jpg",
+            answerPoints: -14
+            },
+            {
+            answer: "[Redacted]",
+            answerPicture:"./grafiki/wrong1999.jpg",
+            answerPoints: -1999
+            }, 
+            {
+            answer: "I turned myself into a pickle!",
+            answerPicture:"./grafiki/neutral.jpg",
+            answerPoints: 0
+            },
+        ],
+        questionPictures: [],
+        answerPictures: [],
+        imageClass: []
+
+    },
+    {
+        question: "Choose your least favourite bird",
+        answers: [],
+        questionPictures: [],
+        answerPictures: [
+            {
+                answer: "./grafiki/emu.jpg",
+                answerPicture:"./grafiki/wrong1999.jpg",
+                answerPoints: -1999
+                }, 
+                {
+                answer: "./grafiki/sparrow.jpg",
+                answerPicture:"./grafiki/correct9736.jpg",
+                answerPoints: 9736
+                },{
+                answer: "./grafiki/Pied-winged_swallow_(Hirundo_leucosoma).jpg",
+                answerPicture:"./grafiki/wrong1.jpg",
+                answerPoints: -1
+                }, 
+                {
+                answer: "./grafiki/goose.jpg",
+                answerPicture:"./grafiki/wrong1999.jpg",
+                answerPoints: -1999
+                }
+        ],
+        imageClass: "small"
+    },
+    {
+        question: "What is your favourite social media?",
+        answers: [],
+        questionPictures: [],
+        answerPictures: [
+            {
+            answer: "./grafiki/tiktok-app-icon-logo-0F5AD7AE01-seeklogo.com.png",
+            answerPicture:"./grafiki/correct69.jpg",
+            answerPoints: 69
+            }, 
+            {
+            answer: "./grafiki/fb.png",
+            answerPicture:"./grafiki/wrong127.jpg",
+            answerPoints: -127
+            },
+            {
+            answer: "./grafiki/inst.png",
+            answerPicture:"./grafiki/wrong14.jpg",
+            answerPoints: -14
+            }, 
+            {
+            answer: "./grafiki/twit.png",
+            answerPicture:"./grafiki/wrong1.jpg",
+            answerPoints: -1
+            }
+        ],
+        imageClass: "small"
+    },
+    {
+        question: "Would you have any problem if your town was used as a nuclear testing place?",
+        answers: [
+            {
+            answer: "No",
+            answerPicture:"./grafiki/correct1836.jpg",
+            answerPoints: 1836
+            }, 
+            {
+            answer: "Yes",
+            answerPicture:"./grafiki/wrong668.jpg",
+            answerPoints: -688
+            },
+        ],
+        questionPictures: [],
+        answerPictures: [],
+    },
+
+]
+let timeRemaining=15
+
+let numQuestion=0
+let currentCredit=0
+let currentQuestion = 0
+const questionsHTML = document.getElementById("questions")
+
+
+function showQuestion(i){
+if(currentQuestion<17){
+    let AnswerButtons = questions[i].answers.map(answer=>`<button class="button"
+    data-picture="${answer.answerPicture}"
+    data-points="${answer.answerPoints}"
+    >${answer.answer}</button>`).join("")
+
+    let QuestionPictures = questions[i].questionPictures.map(picture=>`<div><img class="${questions[i].imageClass}" src="${picture}"></div>`).join("")
+    let AnswerPictures =   questions[i].answerPictures.map(picture=>`<div class="button "><img  class="${questions[i].imageClass}"" src="${picture.answer}"
+    data-picture="${picture.answerPicture}"
+    data-points="${picture.answerPoints}"></div>`).join("")
+
+    
+
+
+    questionsHTML.innerHTML= 
+    `<div class='question'><div class='questionText'>
+    <p class="questioNnum">Question ${numQuestion+1}: ${questions[i].question}</p>
+    </div>
+    <div class='questionPictureContsiner'>
+    ${QuestionPictures}
+    </div>
+    <div class='answerText'>
+    ${AnswerButtons}
+    </div>
+    <div class='answerPictures'>
+    ${AnswerPictures}
+    </div>
+    </div>`
+    
+    buttonFunctionality()
+}
+else{
+    if(currentCredit>37000){
+        xiCalling()
+    }
+    else if(currentCredit<=37000 && currentCredit > 0){
+       labourCamp(4)
+    }
+    else if(currentCredit<0  && currentCredit>=-69420){
+        labourCamp(1)
+    }
+    else if(currentCredit<-69420){
+        labourCamp(2)
+    }
+}
+}
+
+
+showQuestion(0)
+
+
+function buttonFunctionality(){
+    const btns = document.querySelectorAll('.button')
+
+btns.forEach(btn => {
+
+   btn.addEventListener('click', event => {
+        questionsHTML.innerHTML=`<div class="pointScreen"><img src=${event.target.dataset.picture}
+        ></div>`
+        currentQuestion+=1
+        numQuestion+=1  
+        currentCredit+=(parseInt(event.target.dataset.points)) 
+        document.getElementById("score").innerHTML = `Current social credit score: ${currentCredit}`
+        setTimeout(() => {
+            showQuestion(currentQuestion)
+        }, 1000);
+   });
+
+});
+}
+
+let audio = new Audio('./grafiki/Messenger Incoming Call Ringtone .mp3');
+let audio2 = new Audio('./grafiki/Chinese speaking meme .mp3');
+
+
+
+
+
+function xiCalling(){
+    document.body.innerHTML=""
+    audio.play();
+    setTimeout(() => {
+        document.body.innerHTML = 
+        `<div class="timer"></div>
+        <div class="xiCall">
+            <div class="window">
+                <img class="callWindow" src="./grafiki/xiCalling.jpg">
+                <div class="butContainer">
+                <button class="decline" onclick="labourCamp(3)">Decline</button>
+                <button class="accept" onclick="talk()">Accept</button>
+                </div>
+            </div>
+            <div class="text"></div>
+        </div>
+        `
+    }, 800);
+
+    start()
+
+
+}
+
+function talk(){
+    stop()
+    audio.pause()
+    audio2.play()
+
+    let speach = ["Sir ",",", " CCP ", "is ", "very ", "proud ", "of ", "your ", "outstanding ", 
+    "social ", "credit ", "score. ", "We ", "decided ", "that ", "you ", "are ", "trustworthy ", 
+    "and ", "would ", "like ", "to ", "offer ", "you ", "job ", "in ", "People's ", "Bank ", "Of ", "China. " ,
+    "We ", "hope ", "that ", "you ", "will ", "accept ", "this ", "offer. "]
+    let text = document.querySelector(".text")
+    console.log(text)
+    console.log(speach)
+
+    
+    var i = 0;        
+
+function myLoop() {
+  setTimeout(function() {   
+    text.insertAdjacentText("beforeend",speach[i])
+    console.log(i)
+    i++;                   
+    if (i < 38) {          
+      myLoop();            
+    }
+    else{
+        timeRemaining=10
+        start()
+        selectButtons()
+    }                       
+  }, 200)
+}
+
+myLoop();                   
+
+}
+
+
+
+
+
+
+var intervalID;
+
+
+
+// Function to call repeatedly 
+function sayHello(){
+    timeRemaining-=1
+    if(timeRemaining===0){
+        labourCamp(3)
+    }
+    console.log(timeRemaining)
+    document.querySelector(".timer").innerHTML=timeRemaining
+}
+
+// Function to start setInterval call
+function start(){
+    intervalID = setInterval(sayHello, 1000);
+    console.log(timeRemaining)
+}
+
+// Function to stop setInterval call
+function stop(){
+    clearInterval(intervalID);
+}
+
+
+
+function selectButtons(){
+    setTimeout(() => {
+        document.querySelector(".window").innerHTML=
+    `
+    <button class="no" onclick="labourCamp(3)">DECLINE OFFER</button>
+    <button class="yes" onclick="stage2()">ACCEPT OFFER</button>
+    `
+    }, 900);
+}
+
+function labourCamp(scenario){
+    console.log("dupa")
+    audio.pause()
+    stop()
+
+
+
+    document.body.innerHTML=""
+    setTimeout(() => {
+        if(scenario ==1){
+            var speach = ["Sir ",",", " your ", "social ", "credit ", "is ", "low. ","you ", "are ","cringe ","and ", "dissapointment ", "to ", "CCP ", "and ", "you ", "will ", 
+            "go ", "to ", "Xinjiang ", "for ", "reeducation. "]
+            var grafika = "./grafiki/gulag.jpg"
+        }
+        if(scenario ==2){
+            var speach = ["Sir ",",", " your ", "social ", "credit ", "is ", "so ", "low ", "that ", "Chairman ", "Mao ", "rose ", "from ", "the ", "dead ", "and ", "died ", "again ,",
+            "You ",  "have ",  "lowest ",  "social ",  "credit ",  "in ",  "china ",  "and ",  "no ",  "reeducation ",  "can ",  "fix ",  "it. ", "You ",  "will ",  "be ",  "drone-striked. "
+        ]
+        var grafika = "./grafiki/end.jpg"
+    }
+        if(scenario ==3){
+            var speach = ["Sir, ", "you ", "should ", "not ", "have ", "ignored ", "Chairman Xi, ", "all ", "your ", "social ", "credit ", "is ", "taken ", "away ", "and ", "you ", "are ", "declared ", "enemy ", 
+            "of ", "the ", "state "]
+            var grafika = "./grafiki/gulag.jpg"
+        } 
+        if(scenario ==4){
+            var speach = ["Sir ", "your ", "social ", "credit ", "score ", "could ", "have ", "been ", "higher ", "but ", "CCP ", "is ", "pleased ", "with ", "your ", "performence, ", "you ", 
+        "can ", "continue ", "your ", "daily ", "life. "]
+            var grafika = "./grafiki/good.jpg"
+        }
+        
+        audio2.play()
+
+        document.body.innerHTML = 
+        `
+        <div class="xiCall">
+            <div class="window">
+                <img class="callWindow" src="${grafika}">
+                <div class="text"></div>
+                </div>
+            </div>
+        </div>
+        `
+
+
+
+        
+        let text = document.querySelector(".text")
+        console.log(text)
+        console.log(speach)
+        var i = 0;        
+    
+        function myLoop() {
+        setTimeout(function() {   
+        text.insertAdjacentText("beforeend",speach[i])
+        console.log(i)
+        i++;                   
+        if (i < speach.length) {          
+          myLoop();            
+        }                   
+      }, 200)
+    }
+    
+    myLoop();   
+    }, 800);
+
+
+    
+            
+}
+
+
+function stage2(){
+    window.location.href = "./test.html";
+
+}
